ea396a498de71562b7185209027820120eb2f999
jahid hasan rifat <jahadhasanrifat@gmail.com>
2024-09-23 20:30:32 +0600

    some modify

--- js/index.js
+++ js/index.js
@@ -16,9 +16,21 @@ document.getElementById('fristDonatebtn').addEventListener('click',function(){
         const fristDivTotalMoney=totalMenyeFristDiv+fristInput;
         document.getElementById('totalamountfrist').innerText=fristDivTotalMoney;
         const modal = document.getElementById('my_modal_1');
-        modal.showModal();  
-
+        modal.showModal();
+        
+        
     }
+    // const fristInput= getInputValue('fristDonateInput');
+const historyItem=document.createElement("div");
+historyItem.className=
+"bg-white p-3 rounded-md border-2 border-indigo-500 w-8/12 mx-auto";
+historyItem.innerHTML=`
+<p>${fristInput} Taka is Donated for famine-2024 at Feni, Bangladesh</p>
+<p>Date: ${new Date().toLocaleDateString()}</p>    
+
+`
+const historycontainer=document.getElementById('history-item');
+historycontainer.insertBefore(historyItem, historycontainer.firstChild)
 })
 // second div
 
@@ -68,6 +80,30 @@ document.getElementById('thridDonateBtn').addEventListener('click',function(){
 document.getElementById('blog').addEventListener('click',function(){
     window.location.href='/html/blog.html';
 })
+
+
+// history btn
+const donationTab=document.getElementById('donationtab')
+const historyTab=document.getElementById('history-tab')
+historyTab.addEventListener('click',function()
+{
+    historyTab.classList.add("bg-[#B4F461]"
+        ,"rounded-lg",
+        "border-1"
+        ,"border-[#B4F461]"
+        ,"font-semibold",
+        "text-[#111111]");
+    donationTab.classList.remove("bg-[#B4F461]"
+                ,"rounded-lg",
+                "border-1"
+                ,"border-[#B4F461]"
+                ,"font-semibold"
+            ,"text-[#111111]");
+            document.getElementById('mainborder').classList.add("hidden");
+            document.getElementById('history-item').classList.remove("hidden");
+}
+)
+
 
 
 
---
+++ js/modal.js
@@ -0,0 +1,11 @@
+// const fristInput= getInputValue('fristDonateInput');
+// const historyItem=document.createElement("div");
+// historyItem.className=
+// "bg-white p-3 rounded-md border-2 border-indigo-500 w-8/12 mx-auto";
+// historyItem.innerHTML=`
+//     <p>${fristInput} Taka is Donated for famine-2024 at Feni, Bangladesh</p>
+//     <p>Date: ${new Date().toLocaleDateString()}</p>    
+
+// `
+// const historycontainer=document.getElementById('history-item');
+// historycontainer.insertBefore(historyItem, historycontainer.firstChild)
