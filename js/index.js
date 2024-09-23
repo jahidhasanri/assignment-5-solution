0001d3352c1827ae3f9ce2c7fe29b56571de5758
jahid hasan rifat <jahadhasanrifat@gmail.com>
2024-09-24 01:07:46 +0600

    add responsive

--- js/index.js
+++ js/index.js
@@ -21,10 +21,10 @@ document.getElementById('fristDonatebtn').addEventListener('click',function(){
         
         const historyItem=document.createElement("div");
         historyItem.className=
-        "bg-white p-3 rounded-md border-2 border-indigo-500 w-8/12 mx-auto";
+        "bg-white p-3 rounded-md border-2 border-gray-200 mb-5 w-8/12 mx-auto";
         historyItem.innerHTML=`
         <p>${fristInput} Taka is Donated for famine-2024 at Feni, Bangladesh</p>
-        <p>Date: ${new Date().toLocaleDateString()}</p>    
+        <p>Date: ${new Date().toLocaleDateString()} GMT +0600 (Bangladesh Standard Time)</p>    
         
         `
         const historycontainer=document.getElementById('history-item');
@@ -57,10 +57,10 @@ document.getElementById('secondDonateBtn').addEventListener('click',function(){
 
         const historyItem=document.createElement("div");
         historyItem.className=
-        "bg-white p-3 rounded-md border-2 border-indigo-500 w-8/12 mx-auto";
+        "bg-white p-3 rounded-md border-2 border-gray-200 w-8/12 mx-auto mb-5 mt-5";
         historyItem.innerHTML=`
-            <p>${secondInput} Taka is Donated for famine-2024 at dhaka, Bangladesh</p>
-            <p>quasba: ${new Date().toLocaleDateString()}</p>    
+            <p>${secondInput} Taka is Donated for Flood Relief in Feni,Bangladesh</p>
+            <p>Date: ${new Date().toLocaleDateString()} GMT +0600 (Bangladesh Standard Time)</p>    
         
         `
         const historycontainer=document.getElementById('history-item3');
@@ -96,10 +96,10 @@ document.getElementById('thridDonateBtn').addEventListener('click',function(){
 
         const historyItem=document.createElement("div");
         historyItem.className=
-        "bg-white p-3 rounded-md border-2 border-indigo-500 w-8/12 mx-auto";
+        "bg-white p-3 rounded-md border-2 border-gray-200 w-8/12 mx-auto";
         historyItem.innerHTML=`
-            <p>${thridInput} Taka is Donated for famine-2024 at dhaka, Bangladesh</p>
-            <p>Date: ${new Date().toLocaleDateString()}</p>    
+            <p>${thridInput} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</p>
+            <p>Date: ${new Date().toLocaleDateString()} GMT +0600 (Bangladesh Standard Time)</p>    
         
         `
         const historycontainer=document.getElementById('history-item2');
